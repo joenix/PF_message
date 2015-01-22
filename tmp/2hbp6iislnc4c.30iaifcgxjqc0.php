554f7d67efdd01add6d22dc4b3b60284087186e0
joenix <joenix@qq.com>
2015-01-22 17:58:05 +0800

    Finally

--- tmp/2hbp6iislnc4c.30iaifcgxjqc0.php
+++ tmp/2hbp6iislnc4c.30iaifcgxjqc0.php
@@ -1,3 +1,4 @@
 <h2><?php echo $data['title']; ?></h2>
-<h4><?php echo $data['author']; ?></h4>
+<h4>作者：<?php echo $data['author']; ?></h4>
+<h4>最后修改：<?php echo $data['time']; ?></h4>
 <section><?php echo $data['description']; ?></section>
